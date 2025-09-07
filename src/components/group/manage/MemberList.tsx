cbe92d7ac90a5b82e9679055b621f0095b94636b
Amorn Phanturat <amornphanturat@Amorns-MacBook-Air.local>
2025-09-07 13:51:26 +0700

    add real functionality to the group manage

--- src/components/group/manage/MemberList.tsx
+++ src/components/group/manage/MemberList.tsx
@@ -2,10 +2,15 @@ import { Button } from "@/components/ui/button";
 import { Crown, Trash2, Users } from "lucide-react";
 
 interface Member {
-  id: number;
-  name: string;
-  isOwner: boolean;
-  tags?: string[];
+    user_id: number
+    first_name: string
+    middle_name: any
+    last_name: string
+    sex: string
+    profile_url: any
+    interests: string[]
+    travel_styles: string[]
+    isOwner? : boolean
 }
 
 interface Props {
@@ -17,21 +22,21 @@ interface Props {
 export default function MemberList({ members, onDelete, onTransfer }: Props) {
   return (
     <ul className="space-y-8">
-      {members.map((m) => (
+      {members.sort((x,y)=>x.isOwner ? -1 : 1).map((m) => (
         <li
-          key={m.id}
+          key={m.user_id}
           className="flex items-center gap-4 bg-slate-900/80 rounded-xl px-4 py-3 shadow border border-orange-500/10 hover:scale-[1.02] transition-all"
         >
             <div className="flex w-full flex-row px-1 gap-4 justify-between">
                 <div className="flex flex-row w-full gap-4">
                     <div className="h-[50px] flex flex-col items-center justify-center">
                         <div className="w-10 h-10 bg-orange rounded-xl text-xl font-semibold bg-orange-500/80 text-center items-center justify-center flex">
-                            {m.name.charAt(0)}
+                            {m.first_name.charAt(0)}
                         </div>
                     </div>
                     <div className="flex h-full flex-col">
                         <div className="flex flex-row items-center justify-start gap-2">
-                            <p className="text-lg font-semibold">{m.name}</p>
+                            <p className="text-lg font-semibold">{m.first_name} {Array.from(m.last_name).splice(0,2)}.</p>
                             {(m.isOwner)? (
                                 <Crown color="orange" size={18}></Crown>
                             ) : (
@@ -42,12 +47,14 @@ export default function MemberList({ members, onDelete, onTransfer }: Props) {
                     </div>
                 </div>
                 <div className="flex flex-col h-[50px] justify-end items-end">
+                  {(!m.isOwner) && (
                     <div className="flex flex-row h-[30px] gap-2">
-                        <button className="bg-amber-500/80 rounded-md p-1 w-auto font-semibold" onClick={()=>onTransfer(m.id)}>
+                        <button className="bg-amber-500/80 rounded-md p-1 w-auto font-semibold" onClick={()=>onTransfer(m.user_id)}>
                             <p className="text-sm">Promote</p>
                         </button>
-                        <button className="bg-rose-400 rounded-md p-1 h-auto flex items-center justify-center" onClick={()=>{onDelete(m.id)}}><Trash2 size={18}></Trash2></button>
+                        <button className="bg-rose-400 rounded-md p-1 h-auto flex items-center justify-center" onClick={()=>{onDelete(m.user_id)}}><Trash2 size={18}></Trash2></button>
                     </div>
+                  )}
                 </div>
             </div>
         </li>
