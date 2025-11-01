9a506618fd8cdbda28a922d7540b67acd4a42597
Jirapat-K-F <jirapat.flum@gmail.com>
2025-11-01 21:45:06 +0700

    delete account test v2

--- tests/user.test/user.test.ts
+++ tests/user.test/user.test.ts
@@ -120,3 +120,28 @@ test("user cannot update profile with invalid data", async ({ page }) => {
         )
         .click()
 })
+
+test("user can delete account", async ({ page }) => {
+    const testUser = TEST_USERS_DATA.testUser1
+    await page.goto("/")
+    await page.getByRole("button", { name: "✨ Sign In" }).click()
+    await page.getByRole("textbox", { name: "Email" }).click()
+    await page.getByRole("textbox", { name: "Email" }).fill(testUser.email)
+    await page.getByRole("textbox", { name: "Password" }).click()
+    await page
+        .getByRole("textbox", { name: "Password" })
+        .fill(testUser.password)
+    await page.getByRole("button", { name: "✨ Sign In & Explore" }).click()
+    await page.getByRole("button", { name: "J", exact: true }).click()
+    await page.getByRole("menuitem", { name: "Profile Management" }).click()
+
+    await page.getByRole("button", { name: "Delete Account" }).click()
+    await page
+        .getByRole("textbox", { name: "Enter your password to confirm" })
+        .fill("Test8888!")
+    page.once("dialog", (dialog) => {
+        console.log(`Dialog message: ${dialog.message()}`)
+        dialog.dismiss().catch(() => {})
+    })
+    await page.getByRole("button", { name: "Delete Account" }).click()
+})
