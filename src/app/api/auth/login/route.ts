91294b78ff6f0ee9af822ff6edca02cdb624184b
Thanakrit_B <thanakirt47@gmail.com>
2025-11-17 23:24:00 +0700

    modify cookie

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -8,8 +8,8 @@ export async function POST(req: Request) {
         const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/v1/auth/login`, reqBody);
         const ret = response.data;
         const res = new NextResponse();
-        res.cookies.set("accessToken", ret.data.accessToken);
-        res.cookies.set("refreshToken", ret.data.refreshToken);
+        res.cookies.set("accessToken", ret.data.accessToken, { httpOnly: true , secure: true, sameSite: 'lax', path: '/'});
+        res.cookies.set("refreshToken", ret.data.refreshToken, { httpOnly: true , secure: true, sameSite: 'lax', path: '/'});
         return res;
     } catch (error: any) {
         console.error("Login error:", error.response?.data || error.message);
